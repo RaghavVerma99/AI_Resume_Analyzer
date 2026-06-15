3ca7f5eddff0b5bcc57f9fa9f4195f5776bb85ee
Raghav <risshu.verma7@example.com>
2026-06-15 23:31:15 +0530

    Initial commit

--- backend/main.py
+++ backend/main.py
@@ -18,12 +18,16 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
+
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173"
     ],
+
     allow_credentials=True,
+
     allow_methods=["*"],
+
     allow_headers=["*"],
 )
 
